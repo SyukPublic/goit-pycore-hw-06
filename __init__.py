05aa48ea06967170a08619f94511f9a73457f919
roman <galas@incust.com>
2025-06-19 08:58:43 +0300

    Task for virtual assistant added ReadMe edited

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-"
+
+__title__ = 'Home Work 06'
+__author__ = 'Roman'
+
+from tasks.virtual_assistant import main as virtual_assistant_bot
+
+__all__ = ['virtual_assistant_bot']
---
+++ test_virtual_assistant.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-"
+
+"""
+Tests for virtual assistant
+"""
+
+from tasks.virtual_assistant import main as virtual_assistant_bot
+
+
+if __name__ == "__main__":
+    virtual_assistant_bot()
